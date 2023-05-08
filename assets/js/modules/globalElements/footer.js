c85e83200dcdd83747b8113e10b91f1ebffc602f
diarisdiakite <diarisdiakite@gmail.com>
2023-05-08 18:47:23 +0200

    Add html global sections navbar and footer js files

---
+++ assets/js/modules/globalElements/footer.js
@@ -0,0 +1,5 @@
+document.getElementById('footer-section').innerHTML += `
+  <div class="footer-text">
+    <div class="footer-rights"> Copyright @ 2023 Powered by Didy. All Rights Reserved. </div>
+  </div>
+`
---
+++ assets/js/modules/globalElements/lists/list_navItems.js
@@ -0,0 +1,14 @@
+export let navItems = [
+  {
+    id: 'display-list',
+    name: 'List '
+  },
+  {
+    id: 'display-add',
+    name: 'Add new '
+  },
+  {
+    id: 'display-contact',
+    name: 'Contact '
+  }
+]
