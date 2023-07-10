7f13c313fdee5d0c4830c4d6cb47180a1df84a74
diarisdiakite <diarisdiakite@gmail.com>
2023-07-11 00:45:38 +0200

    Remove CRUD functions from class methods.

--- assets/js/modules/classes/bookCollectionClass.js
+++ assets/js/modules/classes/bookCollectionClass.js
@@ -8,39 +8,18 @@ export default class BookCollection {
     this.loadBooksFromStorage();
   }
 
-  addBook(title, author) {
-    const error = 'Please provide a title and an author for the Book';
-    if (title !== '' && author !== '') {
-      const book = new Book(
-        // id,
-        title,
-        author,
-      );
-      this.books.unshift(book);
-      this.saveBooksToStorage();
-      return this.books;
-    }
-    return error;
-  }
-
-  removeBook(id) {
-    this.books = this.books.filter((book) => book.id !== parseInt(id, 10));
-    this.saveBooksToStorage();
-    return this.books;
-  }
-
   getBooksByAuthor(author) {
     return this.books.filter((book) => book.author === author);
   }
 
-  displayBooks() {
+  /* displayBooks() {
     return this.books;
-  }
+  } */
 
   loadBooksFromStorage() {
     const savedBooks = localStorage.getItem(this.name);
     if (savedBooks) {
-      this.books = JSON.parse(savedBooks).map((book) => new Book(book.title, book.author, book.id));
+      this.books = JSON.parse(savedBooks).map((book) => new Book(book.title, book.author, book.id)); //Check this afterwards
     }
     return this.books;
   }
@@ -50,5 +29,5 @@ export default class BookCollection {
   }
 }
 
-export const myBookList = new BookCollection('My Book Collection');
+export const myBookList = new BookCollection('BookData');
 export const books = myBookList.loadBooksFromStorage() || [];
